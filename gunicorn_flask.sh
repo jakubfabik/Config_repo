6e6b816718b00b4b1acf070841afafd2fab55596
jakubfabik <fabikjakub@outlook.com>
2024-08-14 11:12:18 +0200

    odoberam symlinky a pridavam do repo kopie suborov

--- gunicorn_flask.sh
+++ gunicorn_flask.sh
@@ -1,1 +1,4 @@
-/home/eof/scripts/gunicorn_flask.sh
+#!/bin/sh
+APP_DIR="/home/eof/rpc_server"
+
+gunicorn --chdir $APP_DIR --bind 127.0.0.1:5555 rpc_server:app
--- set_pfsense_and_net.sh
+++ set_pfsense_and_net.sh
@@ -1,1 +1,6 @@
-/home/eof/scripts/set_pfsense_and_net.sh
+#!/bin/sh
+vmrun start /home/eof/vmware/Pfsense/Pfsense.vmx nogui &
+sleep 60
+nmcli connection up "Ethernet connection 1"
+sleep 10
+sudo systemctl restart nginx
